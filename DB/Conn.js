30b83254f6e9e5e03f85bf42c714b28937691249
Huzaifa990 <sultanhuzaifa0@gmail.com>
2024-08-18 13:28:38 -0500

    DB added

--- DB/Conn.js
+++ DB/Conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/kelsey-backend").then(()=>{
+mongoose.connect("mongodb+srv://khaas25:1234@wlms.zjs0lk4.mongodb.net/kelsey-backend").then(()=>{
     console.log("Connected to DB");
 }).catch((e)=>{
     console.log(e);
