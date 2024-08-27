1ffb3aac41adcffd29314b8d3b866f90b0879200
Huzaifa990 <sultanhuzaifa0@gmail.com>
2024-08-27 11:51:34 -0500

    Tailwind Css Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='w-[100%] p-[40px] bg-slate-600 flex justify-center items-center'>
+      <h1 className='text-white text-[35px] font-semibold'>Hello World</h1>
     </div>
+
+    <div className='flex justify-around p-[40px]'>
+      <div className='w-[100px] h-[100px] bg-slate-700 rounded-md'></div>
+      <div className='w-[100px] h-[100px] bg-slate-700 rounded-md'></div>
+      <div className='w-[100px] h-[100px] bg-slate-700 rounded-md'></div>
+    </div>
+
+    </>
   );
 }
 
